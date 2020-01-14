bcb591381296c35c0df0fc4594600ea36099c4cb
sargaskim <2103702@talnet.nl>
2020-01-14 14:49:48 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,9 @@
+<?php
+$computter = 'hello, whats your name? '.PHP_EOL;
+echo $computter;
+
+$Fname = readline("First name: ");
+$Lname = readline("Last name: ");
+
+
+echo 'your name is: '.$Fname.' '.$Lname;
